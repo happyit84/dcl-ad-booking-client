f858fdae82eba269502669fccffcea97162d99bf
yeonsik <happyit84@gmail.com>
2022-09-12 17:18:23 +0900

    add "Connect Your Metamask Wallet" button

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +21,58 @@ function App() {
       </header>
     </div>
   );
+}*/
+const App: React.FC = () => {
+  const [isMetamaskInstalled, setIsMetamaskInstalled] = useState<boolean>(false);
+  const [ethereumAccount, setEthereumAccount] = useState<string | null>(null);
+
+  useEffect(() => {
+    if((window as any).ethereum) {
+      // check if Metamask wallet is installed
+      setIsMetamaskInstalled(true);
+    }
+  }, []);
+
+  // Does the User have an Ethereum wallet/account?
+  async function connectMetamaskWallet(): Promise<void> {
+    // to get around type checking
+    (window as any).ethereum
+    .request({
+      method: "eth_requestAccounts",
+    })
+    .then((accounts : string[]) => {
+      setEthereumAccount(accounts[0]);
+    })
+    .catch((error: any) => {
+      alert('Something went wrong: ${error}');
+    });
+  }
+
+  if (ethereumAccount === null) {
+    return (
+      <div className="App App-header">
+        {
+          isMetamaskInstalled ? (
+            <div>
+              <img src={logo} alt="logo" />
+              <button onClick={connectMetamaskWallet}>Connect Your Metamask Wallet</button>
+            </div>
+          ) : (
+            <p>Install Your Metamask wallet</p>
+          )
+        }
+      </div>
+    );
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <p>ETH wallet connected as: {ethereumAccount}</p>
+      </header>
+    </div>
+  )
 }
 
 export default App;
